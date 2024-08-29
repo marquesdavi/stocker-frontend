3c89cef35a75945873995771a2a9b5b46cd9db3f
davimarques <davibaterabatera@hotmail.com>
2024-08-29 20:52:25 -0300

    MIZERAAAAA MM

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
+import { OrdersComponent } from './orders/orders.component';
 
 export const routes: Routes = [
     {path: '', component: ProductsComponent},
-    {path: 'products', component: ProductsComponent}
+    {path: 'products', component: ProductsComponent},
+    {path: 'orders', component: OrdersComponent}
 ];
